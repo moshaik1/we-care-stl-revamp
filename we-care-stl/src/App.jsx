978fad6f141d8e8ccedb0fcd877831cce70966fe
Mohsin Shaik <mohsinrshaik@gmail.com>
2023-11-08 14:01:58 -0600

    need to fix errors

--- we-care-stl/src/App.jsx
+++ we-care-stl/src/App.jsx
@@ -1,6 +1,4 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
 
 
 function App() {
@@ -14,3 +12,4 @@ function App() {
 }
 
 export default App
+
